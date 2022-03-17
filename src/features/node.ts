9eb6adc723e27ea24eeb24ae5df5dd46d5d57d28
Vasily Kovnev <54976219+KaguraDun@users.noreply.github.com>
2022-03-17 18:03:52 +0300

    feat: add get rootNode reducer

--- src/features/node.ts
+++ src/features/node.ts
@@ -1,9 +1,28 @@
 /* eslint-disable no-param-reassign */
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+import NodeApi from '@/services/NodeApi';
+
+const api = new NodeApi();
+
+export const getRootNode = createAsyncThunk(
+  'node/getRootNode',
+  api.getRootNode
+);
+
 const nodeSlice = createSlice({
   name: 'node',
   initialState: {
     nodeList: {},
+  },
+  reducers: {},
+  extraReducers: {
+    [getRootNode.fulfilled]: (state, action) => {
+      const [newNode] = action.payload;
+      const { id } = newNode;
+
+      state.nodeList[id] = newNode;
+    },
   },
 });
 
