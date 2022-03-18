94ca1bb5d0889415304f1465d7ec2e3f0fe81774
Vasily Kovnev <54976219+KaguraDun@users.noreply.github.com>
2022-03-18 17:48:49 +0300

    feat: add home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import TreeViewer from '@/components/TreeViewer/TreeViewer';
+
+import s from './Home.scss';
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      <main className={s.content}>
+        <TreeViewer />
+      </main>
+    </div>
+  );
 }
 
 export default Home;
