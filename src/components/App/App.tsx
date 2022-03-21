e247144534e8cfd159fbbc95091b9f8456f4a98a
Vasily Kovnev <54976219+KaguraDun@users.noreply.github.com>
2022-03-21 12:53:44 +0300

    feat: add header and footer

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,19 +1,22 @@
-import './App.scss';
-
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Home from '@/pages/Home/Home';
 import PageNotFound from '@/pages/PageNotFound/PageNotFound';
 
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+
 const App = () => (
   <>
-    <div className="container">
+    <Header />
+    <main className="container d-flex aligns-items-center justify-content-center">
       <Switch>
         <Route component={Home} exact path="/" />
         <Route component={PageNotFound} />
       </Switch>
-    </div>
+    </main>
+    <Footer />
   </>
 );
 
