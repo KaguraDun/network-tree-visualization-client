56ad727f1eb81daa10a24989ff97a8b9a677fe70
Vasily Kovnev <54976219+KaguraDun@users.noreply.github.com>
2022-03-21 21:01:02 +0300

    feat: add types for create tree helper

--- src/helpers/createTree.ts
+++ src/helpers/createTree.ts
@@ -1,14 +1,27 @@
-const createTree = (nodeList) => {
-  const tree = [];
-  const childrenTable = {};
+import { NodeElement } from '@/models/Node';
+
+type NodeElementWithoutChildren = Omit<NodeElement, 'children'>;
+type NodeElementWithChildrenElements = NodeElementWithoutChildren & {
+  children: NodeElementWithoutChildren[];
+};
+
+type ChildrenTable = Record<number, NodeElementWithChildrenElements>;
+
+const createTree = (nodeList: NodeElement[]) => {
+  const tree: NodeElement[] = [];
+  const childrenTable: ChildrenTable = {};
 
   nodeList.forEach((node) => {
-    console.log(node);
-    childrenTable[node.id] = node;
-    childrenTable[node.id].children = [];
+    const { children, ...rest } = node;
+    const withoutChildren: NodeElementWithChildrenElements = {
+      ...rest,
+      children: [],
+    };
+
+    childrenTable[node.id] = withoutChildren;
   });
 
-  nodeList.forEach((node) => {
+  nodeList.forEach((node: NodeElement) => {
     if (node.parentID !== null) {
       childrenTable[node.parentID].children.push(node);
     } else {
