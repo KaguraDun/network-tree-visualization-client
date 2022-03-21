05eaeaa6748e18cc118cac36d8ea36abd57d0e90
Vasily Kovnev <54976219+KaguraDun@users.noreply.github.com>
2022-03-21 15:42:35 +0300

    feat: update home page style

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -7,11 +7,11 @@ import TreeViewer from '@/components/TreeViewer/TreeViewer';
 
 function Home() {
   return (
-    <div className="row">
-      <div className="col overflow-auto">
+    <div className="row mb-5">
+      <div className="col-sm-7 col-md-6 overflow-auto">
         <TreeViewer />
       </div>
-      <div className="col ">
+      <div className="col-sm-5 col-md-4">
         <NodeInfo />
       </div>
     </div>
